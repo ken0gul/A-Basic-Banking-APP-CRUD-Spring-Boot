85cab4b6907500171fee42d99f47f305a3fb6d11
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-05 14:15:05 -0500

    Add an overloaded SaveUser method

--- src/main/java/com/coderscampus/assignment13/service/UserService.java
+++ src/main/java/com/coderscampus/assignment13/service/UserService.java
@@ -9,30 +9,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coderscampus.assignment13.domain.Account;
+import com.coderscampus.assignment13.domain.Address;
 import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.repository.AccountRepository;
+import com.coderscampus.assignment13.repository.AddressRepository;
 import com.coderscampus.assignment13.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepo;
 	@Autowired
 	private AccountRepository accountRepo;
-	
+
+	@Autowired
+	private AddressRepository addressRepository;
+
+	// Address
+
+	public Address findAddressById(User user) {
+		Long userId = user.getUserId();
+		Address address = addressRepository.findAddressById(userId);
+
+		if (address == null) {
+			address = new Address();
+		}
+
+		return address;
+	}
+
 	public List<User> findByUsername(String username) {
 		return userRepo.findByUsername(username);
 	}
-	
+
 	public List<User> findByNameAndUsername(String name, String username) {
 		return userRepo.findByNameAndUsername(name, username);
 	}
-	
+
 	public List<User> findByCreatedDateBetween(LocalDate date1, LocalDate date2) {
 		return userRepo.findByCreatedDateBetween(date1, date2);
 	}
-	
+
 	public User findExactlyOneUserByUsername(String username) {
 		List<User> users = userRepo.findExactlyOneUserByUsername(username);
 		if (users.size() > 0)
@@ -40,11 +58,11 @@ public class UserService {
 		else
 			return new User();
 	}
-	
-	public Set<User> findAll () {
+
+	public Set<User> findAll() {
 		return userRepo.findAllUsersWithAccountsAndAddresses();
 	}
-	
+
 	public User findById(Long userId) {
 		Optional<User> userOpt = userRepo.findById(userId);
 		return userOpt.orElse(new User());
@@ -52,22 +70,35 @@ public class UserService {
 
 	public User saveUser(User user) {
 		if (user.getUserId() == null) {
+
 			Account checking = new Account();
 			checking.setAccountName("Checking Account");
 			checking.getUsers().add(user);
 			Account savings = new Account();
 			savings.setAccountName("Savings Account");
 			savings.getUsers().add(user);
-			
+
 			user.getAccounts().add(checking);
 			user.getAccounts().add(savings);
 			accountRepo.save(checking);
 			accountRepo.save(savings);
+
 		}
 		return userRepo.save(user);
 	}
 
+	public User saveUser(User user, Address address) {
+
+
+		user.setAddress(address);
+		address.setUser(user);
+		addressRepository.save(address);
+
+		return userRepo.save(user);
+	}
+
 	public void delete(Long userId) {
+		
 		userRepo.deleteById(userId);
 	}
 }
