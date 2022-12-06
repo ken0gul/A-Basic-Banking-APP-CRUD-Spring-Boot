630759ca8bd7f9b2183b1a2b257b6a54b64cb2f1
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-06 11:55:22 -0500

    Add findByAccountId to the AccountRepo

--- src/main/java/com/coderscampus/assignment13/repository/AccountRepository.java
+++ src/main/java/com/coderscampus/assignment13/repository/AccountRepository.java
@@ -1,6 +1,9 @@
 package com.coderscampus.assignment13.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.coderscampus.assignment13.domain.Account;
@@ -8,4 +11,6 @@ import com.coderscampus.assignment13.domain.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
 
+	
+	Account findByAccountId(Long accountId);
 }
