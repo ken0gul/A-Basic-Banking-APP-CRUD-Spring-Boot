4f62030406ba011a3f9eb6c388c69c90564e6635
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-12-07 16:31:30 -0500

    Add conditionals for the modelmap according to allAccounts size

--- src/main/java/com/coderscampus/assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/assignment13/web/UserController.java
@@ -23,8 +23,8 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
-	private Set<Account> accounts = new HashSet<Account>();
+
+	private List<Account> accounts = new ArrayList<Account>();
 
 	@GetMapping("/register")
 	public String getCreateUser(ModelMap model) {
@@ -70,8 +70,11 @@ public class UserController {
 
 	@PostMapping("/users/{userId}")
 	public String postOneUser(User user, Address address) {
-
+		User foundUser = userService.findById(user.getUserId());
+		user = foundUser;
+		List<Account> accounts = foundUser.getAccounts();
 		userService.saveUser(user, address);
+		System.out.println(user);
 
 		return "redirect:/users/" + user.getUserId();
 	}
@@ -83,105 +86,88 @@ public class UserController {
 	}
 
 	// Account page
-	
-	
-	@GetMapping("/users/{userId}/account")
-	public String getCreateAccount(ModelMap model) {
-	
-		model.put("accounts", new Account());
-		
-		
-		return "account";
-	}
 
-	
-	
-	@PostMapping("/users/{userId}/account")
-	public String createAccount(Account account, @PathVariable Long userId, @PathVariable Long accountId) {
+	@GetMapping("/users/{userId}/accounts")
+	public String getCreateAccount(ModelMap model, @PathVariable Long userId) {
 		User user = userService.findById(userId);
-		Account savedAccount = userService.saveAccount(account);
+		List<Account> allAccounts = userService.findAllAccounts();
+
+		Integer counter = 0;
+		counter = user.getAccounts().size()+1;
+		model.put("counter", counter);
 		
 		
-		if(accounts == null) {
+		if(allAccounts.size() == 0) {
 			
-			accounts.add(account);
-			user.setAccounts(accounts);
+			model.put("accounts",new Account());
+		} else {
+			model.put("accounts", allAccounts.get(allAccounts.size()-1));
 		}
 		
 		
-			Set<Account> userAccounts = user.getAccounts();
-			userAccounts.add(account);
+
+		return "account";
+	}
+	//Create account
+	@PostMapping("/users/{userId}/accounts")
+	public String createAccount(Account account, @PathVariable Long userId) {
+		User user = userService.findById(userId);
+		Account savedAccount = userService.saveAccount(account);
+		if (accounts == null) {
+			accounts.add(account);
+			user.setAccounts(accounts);
+		}
+
+		List<Account> userAccounts = user.getAccounts();
 		
-			
+		userAccounts.add(account);
+
 //			user.getAccounts().stream().forEach(item -> System.out.println(item.getAccountName()));
-			
-			
-			user.setAccounts(userAccounts);
-	
-		
-		
-		
-			
-		
-		
-		
+
+		user.setAccounts(userAccounts);
+
 		List<User> users = new ArrayList<>();
 		users.add(user);
-		
-		
+
 		account.setUsers(users);
 //		savedAccount.getUsers().stream().forEach(item -> System.out.println(item.getName()));
 		System.out.println(user);
-	
 		userService.saveUserAccount(user);
-		
-		
-		return "redirect:/users/{userId}/accounts/{accountId}";
-	}
-	
-	
-	
-	@GetMapping("/users/{userId}/accounts/{accountId}")
-	public String getAccounts(ModelMap map, @PathVariable Long userId, @PathVariable Long accountId) {
 
+		return "redirect:/users/{userId}/accounts";
+	}
+
+	@GetMapping("/users/{userId}/accounts/{accountId}")
+	public String getAccounts(ModelMap model, @PathVariable Long userId, @PathVariable Long accountId) {
+		User user = userService.findById(userId);
+		Integer counter = 0;
+		counter = user.getAccounts().size();
+		model.put("counter", counter);
 		Account account = userService.findAccountByUserId(accountId);
-		map.put("accounts", account);
+		model.put("accounts", account);
+	
 		return "account";
 	}
-//
+
+	// Modify Accounts
 	@PostMapping("/users/{userId}/accounts/{accountId}")
-	public String createAccounts(@PathVariable Long userId, @PathVariable Long accountId, Account account) {
+	public String createAccounts(ModelMap map, @PathVariable Long userId, @PathVariable Long accountId,
+			Account account, Integer counter) {
 
 		User user = userService.findById(userId);
 		Account savedAccount = userService.saveAccount(account);
+		List<Account> userAccounts = user.getAccounts();
 		
 		
-		
-		
-			Set<Account> userAccounts = user.getAccounts();
-			userAccounts.add(account);
-		
-			
-//			user.getAccounts().stream().forEach(item -> System.out.println(item.getAccountName()));
-			
-			
-			user.setAccounts(userAccounts);
-	
-		
-		
-		
-			
-		
-		
-		
+//		userAccounts.add(account);
+
+		user.setAccounts(userAccounts);
+
 		List<User> users = new ArrayList<>();
 		users.add(user);
-		
-		
+
 		account.setUsers(users);
-//		savedAccount.getUsers().stream().forEach(item -> System.out.println(item.getName()));
-		System.out.println(user);
-	
+
 		userService.saveUserAccount(user);
 
 		return "redirect:/users/{userId}/accounts/{accountId}";
